6e7d4af1e21c55ddcd2245af69c5ff3221869d16
adam <chenzd123456@gmail.com>
2025-03-14 16:17:16 +0800

    移除未使用的导入，简化 main.tsx 文件，保持代码整洁

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
-import { COLORS, COMMON_STYLE } from "@/common_style";
-import { ZButton, ZColumn, ZRow } from "@/components";
-import { Render, View } from "lvgljs-ui";
-import React from "react";
-import { useNavigate } from "react-router-native";
 import AppRouter from "@/router";
+import { Render } from "lvgljs-ui";
+import React from "react";
 
 Render.render(<AppRouter />);
