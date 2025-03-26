37d6b803c86ba30f7e420b5ce6694a942b2b6a7d
adam <chenzd123456@gmail.com>
2025-03-26 16:20:21 +0800

    重构 App 组件，将 Profiler 移动到渲染函数外部，优化性能和可读性

--- src/main.tsx
+++ src/main.tsx
@@ -4,28 +4,30 @@ import { Dimensions, Render } from "lvgljs-ui";
 import React, { Profiler } from "react";
 import { MemoryRouter, Route, Routes } from "react-router-native";
 
+const { window } = Dimensions;
+
 const App = () => {
-  const { window } = Dimensions;
-
-  const onRender = (id, phase, actualDuration) => {
-    console.log(
-      `${id} 组件 ${phase} 阶段耗时：${actualDuration}ms 分辨率：${window.width}x${window.height}`,
-    );
-  };
-
   return (
-    <Profiler id="AppRouter" onRender={onRender}>
-      <MemoryRouter initialEntries={["/"]} initialIndex={0}>
-        <ZNavScreenLayout>
-          <Routes>
-            {routerData.map(({ name, path, element }) => (
-              <Route id={name} path={path} element={element} />
-            ))}
-          </Routes>
-        </ZNavScreenLayout>
-      </MemoryRouter>
-    </Profiler>
+    <MemoryRouter initialEntries={["/"]} initialIndex={0}>
+      <ZNavScreenLayout>
+        <Routes>
+          {routerData.map(({ name, path, element }) => (
+            <Route id={name} path={path} element={element} />
+          ))}
+        </Routes>
+      </ZNavScreenLayout>
+    </MemoryRouter>
   );
 };
 
-Render.render(<App></App>);
+const onRender = (id, phase, actualDuration) => {
+  console.log(
+    `${id} 组件 ${phase} 阶段耗时：${actualDuration}ms 分辨率：${window.width}x${window.height}`,
+  );
+};
+
+Render.render(
+  <Profiler id="App" onRender={onRender}>
+    <App />
+  </Profiler>,
+);
