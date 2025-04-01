1203d45e46e20d8ba675479954e1b19ada628822
adam <chenzd123456@gmail.com>
2025-04-01 10:11:47 +0800

    优化 ZInput 组件，重构 ClearButton 逻辑，新增对齐功能，提升组件可用性和可读性

--- src/components/ZInput.tsx
+++ src/components/ZInput.tsx
@@ -1,7 +1,7 @@
-import { ZButton, ZIconSymbol, ZRow, ZSizeEnum, ZStyleProps } from ".";
+import { ZButton, ZIconSymbol, ZSizeEnum, ZStyleProps } from ".";
 import { COMMON_STYLE, CONSTANTS } from "@/common_style";
 import { useMergeStyle } from "@/hooks/styleHooks";
-import { Input } from "lvgljs-ui";
+import { EAlignType, Input, View } from "lvgljs-ui";
 import React, { useEffect, useMemo, useRef, useState } from "react";
 
 const mergeStyle = useMergeStyle();
@@ -25,30 +25,27 @@ interface ZInputProps {
 const style = {
   input: mergeStyle(COMMON_STYLE.noBorder),
   view: mergeStyle(
-    COMMON_STYLE.flexRow,
-    COMMON_STYLE.alignItemsCenter,
-    COMMON_STYLE.justifyContentCenter,
     COMMON_STYLE.autoWidth,
-    COMMON_STYLE.autoHeight,
     COMMON_STYLE.radius4,
     COMMON_STYLE.border1,
+    COMMON_STYLE.padding0,
   ),
 };
 
 const sizeStyleMap: Record<string, ZStyleProps> = {
   small: mergeStyle(
     COMMON_STYLE.minWidth36,
-    COMMON_STYLE.minHeight36,
+    COMMON_STYLE.height36,
     COMMON_STYLE.fontSizeSmall,
   ),
   default: mergeStyle(
     COMMON_STYLE.minWidth40,
-    COMMON_STYLE.minHeight40,
+    COMMON_STYLE.height40,
     COMMON_STYLE.fontSizeDefault,
   ),
   large: mergeStyle(
     COMMON_STYLE.minWidth48,
-    COMMON_STYLE.minHeight48,
+    COMMON_STYLE.height48,
     COMMON_STYLE.fontSizeLarge,
   ),
 };
@@ -61,28 +58,20 @@ interface ClearButtonProps {
 
 const ClearButton = React.memo<ClearButtonProps>(
   ({ size, onClick, display = true }) => {
-    if (display) {
-      return (
-        <ZButton
-          size={size}
-          onClick={onClick}
-          icon={ZIconSymbol.Backspace}
-          round
-          text
-        ></ZButton>
-      );
-    } else {
-      return (
-        <ZButton
-          size={size}
-          icon={ZIconSymbol.Backspace}
-          round
-          text
-          disable
-          style={{ opacity: 0 }}
-        ></ZButton>
-      );
-    }
+    return (
+      <ZButton
+        size={size}
+        icon={ZIconSymbol.Backspace}
+        onClick={onClick}
+        round
+        text
+        disable={!display}
+        style={!display && { display: "none" }}
+        align={{
+          type: EAlignType.ALIGN_RIGHT_MID,
+        }}
+      ></ZButton>
+    );
   },
 );
 
@@ -99,7 +88,6 @@ const ZInput = (props: ZInputProps) => {
   } = props;
 
   const [input, setInput] = useState(value);
-  const inputRef = useRef();
 
   useEffect(() => {
     if (onChange) {
@@ -120,9 +108,8 @@ const ZInput = (props: ZInputProps) => {
   };
 
   return (
-    <ZRow style={style.view} gap={0}>
+    <View style={mergeStyle(style.view, computedInputStyle)}>
       <Input
-        // ref={inputRef}
         style={mergeStyle(style.input, computedInputStyle)}
         placeholder={placeholder}
         maxlength={maxLength}
@@ -133,13 +120,16 @@ const ZInput = (props: ZInputProps) => {
         onFocusStyle={COMMON_STYLE.noBorder}
         autoKeyBoard={true}
         mode={mode}
+        align={{
+          type: EAlignType.ALIGN_CENTER,
+        }}
       ></Input>
       <ClearButton
         onClick={clearInput}
         size={size}
         display={allowClean && input && input.length > 0}
       />
-    </ZRow>
+    </View>
   );
 };
 
