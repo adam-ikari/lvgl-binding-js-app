5d3004b3d91b4d00f6dd02aab4d2c584441edb6f
adam <chenzd123456@gmail.com>
2025-04-02 18:23:56 +0800

    新增 ProcessBarScreen 和 PageSession 组件，提供进度条和页面会话功能

---
+++ src/screens/ProcessBarScreen.tsx
@@ -0,0 +1,9 @@
+import { useMergeStyle } from "@/hooks/styleHooks";
+import { ProgressBar } from "lvgljs-ui";
+import React from "react";
+
+const ProcessBarScreen = () => {
+  return <></>;
+};
+
+export default ProcessBarScreen;
