382d7342d762de56396a001462a07d1577b253fa
adam <chenzd123456@gmail.com>
2025-06-16 19:54:22 +0800

    feat: 在 ImageDemoScreen 中添加 useEffect，打印小图和中图的 URL

--- src/screens/ImageDemoScreen.tsx
+++ src/screens/ImageDemoScreen.tsx
@@ -3,9 +3,14 @@ import { ZSizeEnum } from "../components/common";
 import PageSession from "./common/PageSession";
 import smallImg from "@/assets/images/demo-images/small.jpg";
 import mediumImg from "@/assets/images/demo-images/medium.jpg";
-import React from "react";
+import React, { useEffect } from "react";
 
 const ImageDemoScreen = () => {
+  useEffect(() => {
+    console.log('Small image URL:', smallImg);
+    console.log('Medium image URL:', mediumImg);
+  }, []);
+
   const demoImages = [
     {
       src: smallImg,
@@ -35,9 +40,9 @@ const ImageDemoScreen = () => {
 
   return (
     <PageSession title="图片组件演示">
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+
         {demoImages.map((img, index) => (
-          <div key={index} style={{ textAlign: "center" }}>
+         
             <ZImage
               src={img.src}
               size={img.size === "small" ? ZSizeEnum.Small :
@@ -46,10 +51,9 @@ const ImageDemoScreen = () => {
               round={img.round}
               onClick={() => console.log(`点击了${img.label}`)}
             />
-            <p style={{ marginTop: "8px" }}>{img.label}</p>
-          </div>
+       
         ))}
-      </div>
+      
     </PageSession>
   );
 };
