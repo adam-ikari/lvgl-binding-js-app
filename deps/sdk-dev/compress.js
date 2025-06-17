9d6847d663fd2709ae2d6c6c94b4b9dc61aab12f
adam <chenzd123456@gmail.com>
2025-06-17 11:47:08 +0800

    优化依赖，改进打包压缩规则

--- deps/sdk-dev/compress.js
+++ deps/sdk-dev/compress.js
@@ -12,30 +12,38 @@ const defaultCompressOptions = {
     drop_console: true,
     drop_debugger: true,
     evaluate: true,
-    inline: true,
+    inline: 3,  // 更激进的inline优化
     reduce_vars: true,
     toplevel: true,
-    passes: 3,
+    passes: 5,  // 增加pass次数
     hoist_funs: true,
     hoist_vars: true,
     loops: true,
     join_vars: true,
     unused: true,
     collapse_vars: true,
-    reduce_funcs: true
+    reduce_funcs: true,
+    if_return: true,  // 新增if_return优化
+    pure_funcs: ['console.log'],  // 标记纯函数
+    pure_getters: true  // 假设getter是纯的
   },
   mangle: {
     toplevel: true,
-    keep_fnames: false
+    keep_fnames: false,
+    properties: {  // 新增属性名混淆
+      regex: /^_/  // 混淆以下划线开头的属性
+    }
   },
   format: {
     beautify: false,
     semicolons: false,
-    max_line_len: 1024,
+    max_line_len: 80,  // 更小的行长度
     comments: false,
-    wrap_iife: false,
-    indent_level: 0
-  }
+    wrap_iife: true,  // 包裹立即执行函数
+    indent_level: 0,
+    ecma: 2020  // 指定ECMAScript版本
+  },
+  ecma: 2020  // 指定解析的ECMAScript版本
 };
 
 async function compressFile(inputPath, outputPath, options = {}) {
