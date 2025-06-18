2e2f626cd72a19b4bd9266c994e3e3867e69744b
adam <chenzd123456@gmail.com>
2025-06-18 13:45:36 +0800

    feat: 优化 WorkerDemoScreen 组件，简化 Worker 创建和消息处理逻辑

--- src/screens/WorkerDemoScreen.tsx
+++ src/screens/WorkerDemoScreen.tsx
@@ -4,27 +4,26 @@ import React, { useState } from "react";
 
 const WorkerDemoScreen = () => {
   const [result, setResult] = useState<string>("");
-  let worker: Worker | null = null;
 
   const handleCalculate = () => {
+    console.log("hello!");
 
-    console.log('hello!');
-
-    const w = new Worker('./worker1.worker.js');
-    w.addEventListener('message', event => {
-        const msg = event.data;
-        console.log('received message!');
-        console.log(JSON.stringify(msg));
-        w.terminate();
+    const w = new Worker("./worker1.worker.js");
+    w.addEventListener("message", (event) => {
+      const msg = event.data;
+      console.log("received message!");
+      console.log(JSON.stringify(msg));
+      setResult(JSON.stringify(msg));
+      w.terminate();
     });
 
-    console.log('Worker created!');
+    console.log("Worker created!");
   };
 
   return (
     <PageSession title="Worker Demo">
       <ZButton onClick={handleCalculate}>Calculate with Worker</ZButton>
-      {result && <ZText>{result}</ZText>}
+      <ZText>{result}</ZText>
     </PageSession>
   );
 };
