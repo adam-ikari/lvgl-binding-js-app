36fff6ab7fa30382789ad2be2021b1fce083a8dd
adam <chenzd123456@gmail.com>
2025-06-19 11:02:09 +0800

    fix: 移动 mergeStyle 的定义位置

--- src/layouts/ZNavScreenLayout.tsx
+++ src/layouts/ZNavScreenLayout.tsx
@@ -13,8 +13,6 @@ import { COLORS, COMMON_STYLE } from "@/styles/common_style";
 import React, { useRef } from "react";
 import { EAlignType, View } from "sdk-ui";
 
-const mergeStyle = useMergeStyle();
-
 interface ZNavScreenLayoutProps {
   children?: React.ReactNode | React.ReactNode[];
 }
@@ -22,6 +20,7 @@ interface ZNavScreenLayoutProps {
 const ZNavScreenLayout = (props: ZNavScreenLayoutProps) => {
   const { children } = props;
 
+  const mergeStyle = useMergeStyle();
   const topElementRef = useRef();
 
   return (
