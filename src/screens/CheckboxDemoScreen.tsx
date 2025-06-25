9a17987415946e7dc18ddcff6ed673a08c61e2db
adam <chenzd123456@gmail.com>
2025-06-25 18:42:33 +0800

    refactor: 简化 CheckboxDemoScreen 组件，移除未使用的状态和多余的代码

--- src/screens/CheckboxDemoScreen.tsx
+++ src/screens/CheckboxDemoScreen.tsx
@@ -1,46 +1,21 @@
 import PageSession from "./common/PageSession";
-import { ZCheckbox, ZRow, ZSizeEnum } from "@/components";
+import { ZCheckbox, ZRow } from "@/components";
 import { ZFlexAlignItems } from "@/components/ZFlexContainer";
 import React, { useState } from "react";
 
 const CheckboxDemoScreen = () => {
-  const [checked, setChecked] = useState(false);
   return (
     <>
-      <PageSession title="Size Demo">
-        <ZRow alignItems={ZFlexAlignItems.Center} gap={16}>
-          {[
-            { size: ZSizeEnum.Small, text: "Small" },
-            { size: ZSizeEnum.Default, text: "Default" },
-            { size: ZSizeEnum.Large, text: "Large" },
-          ].map(({ size, text }, index) => (
-            <ZRow key={index} alignItems={ZFlexAlignItems.Center} gap={8}>
-              <ZCheckbox
-                value={checked}
-                onChange={setChecked}
-                size={size}
-                text={text}
-              />
-            </ZRow>
-          ))}
-        </ZRow>
-      </PageSession>
-
-      <PageSession title="State Demo">
+      <PageSession title="Basic">
         <ZRow alignItems={ZFlexAlignItems.Center} gap={16}>
           <ZRow alignItems={ZFlexAlignItems.Center} gap={8}>
-            <ZCheckbox value={true} onChange={setChecked} text="Checked" />
+            <ZCheckbox value={true} text="Checked" />
           </ZRow>
           <ZRow alignItems={ZFlexAlignItems.Center} gap={8}>
-            <ZCheckbox value={false} onChange={setChecked} text="Unchecked" />
+            <ZCheckbox value={false} text="Unchecked" />
           </ZRow>
           <ZRow alignItems={ZFlexAlignItems.Center} gap={8}>
-            <ZCheckbox
-              value={true}
-              onChange={setChecked}
-              disabled
-              text="Disabled"
-            />
+            <ZCheckbox value={true} disabled text="Disabled" />
           </ZRow>
         </ZRow>
       </PageSession>
