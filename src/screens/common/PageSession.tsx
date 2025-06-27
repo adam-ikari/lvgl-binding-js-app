1b151183a0848219b0346a5eeb7d16ea83db8d10
adam <chenzd123456@gmail.com>
2025-06-27 14:13:07 +0800

    fix: 重构 PageSession 组件，移除 ZRow 包裹并优化样式合并逻辑

--- src/screens/common/PageSession.tsx
+++ src/screens/common/PageSession.tsx
@@ -1,26 +1,19 @@
-import {
-  ZCard,
-  ZColumn,
-  ZRow,
-  ZSizeEnum,
-  ZText,
-  ZWidthEnum,
-} from "@/components";
+import { ZCard, ZColumn, ZSizeEnum, ZText, ZWidthEnum } from "@/components";
 import { useMergeStyle } from "@/hooks/styleHooks";
 import { COMMON_STYLE } from "@/styles/common_style";
 import React from "react";
 
-const PageSession = ({ children, title, ...restProps }) => {
-  const mergeStyle = useMergeStyle();
+const mergeStyle = useMergeStyle();
 
+const PageSession = (props) => {
+  const { children, title, style: propsStyle, ...restProps } = props;
   return (
-    <ZRow width={ZWidthEnum.Full} {...restProps}>
-      <ZCard
-        style={COMMON_STYLE.fullWidth}
-        header={<ZText size={ZSizeEnum.Large}>{title}</ZText>}
-        content={<ZColumn width={ZWidthEnum.Full}>{children}</ZColumn>}
-      ></ZCard>
-    </ZRow>
+    <ZCard
+      {...restProps}
+      style={mergeStyle(COMMON_STYLE.fullWidth, propsStyle)}
+      header={<ZText size={ZSizeEnum.Large}>{title}</ZText>}
+      content={<ZColumn width={ZWidthEnum.Full}>{children}</ZColumn>}
+    ></ZCard>
   );
 };
 
